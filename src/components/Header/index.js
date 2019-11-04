8ae43ff53267b55a46b31dc9b9f05edd82f93677
vinnyfs89 <viniciusfesil@gmail.com>
2019-11-04 07:37:40 -0300

    [UPDATE]  AJuste de estrutura de componente

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,14 +2,8 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <h1>Hello</h1>
+        <header id="main-header">JSHunt</header>
     );
 };
 
-// O react reconhece a definição de funções como classes que herdam de Components
-// O Código acima equivale ao código debaixo.
-// class Header extends Component {
-//     render() {
-//         return <h1>Hellow</h1>;
-//     }
-// }
+export default Header;

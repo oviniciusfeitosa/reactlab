d467863e7648b52038e852c8559fe4079aac36c0
vinnyfs89 <viniciusfesil@gmail.com>
2020-02-03 01:20:53 -0300

    :sparkles: Set initial page and api

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import "./styles.css";
 
 import Header from './components/Header';
 
+import Main from './pages/main'
+
 const App = () => (
   <div className="App">
     <Header />
+    <Main />
   </div>
 );
 
---
+++ src/pages/main/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+import api from '../../services/api';
+
+export default class Main extends Component {
+    render() {
+        return <h1>Hello initial page</h1>
+    }
+}
